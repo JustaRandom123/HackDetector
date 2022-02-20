abd6326432bd421cb0263e32dc263577be88d4b2
JustaRandom123 <47950815+JustaRandom123@users.noreply.github.com>
2022-02-20 23:57:51 +0100

    Add files via upload

---
+++ GTA5HackDetector/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTA5HackDetector
+{
+    class Program
+    {
+        public static string path = string.Empty;
+        public static bool md5YesOrNo = false;
+        public static ArrayList possibleHacks = new ArrayList();
+        public static ArrayList foundhacksbymd5 = new ArrayList();
+        public static string blacklistfilePath = AppDomain.CurrentDomain.BaseDirectory + "blacklist.txt";
+        public static string hashlistfilePath = AppDomain.CurrentDomain.BaseDirectory + "hashlist.txt";
+
+
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+
+
+        static void Main(string[] args)
+        {
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            Console.WriteLine("Durch die benutzung von diesem Tool stimmts du zu das dieses Programm berechtigt ist auf sämtliche dateien bzw ordner zuzugreifen und diese auf modding tools zu überprüfen! Das tool kopiert keine dateien oder sammelt Persönliche informationen\nDrücke die 'Enter' taste um dies zu bestätigen");
+            var pressedKey = Console.ReadKey();
+            if (pressedKey.Key != ConsoleKey.Enter)
+            {
+                return;
+            }
+            Console.Clear();
+         
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("Hack detector by JustaRandom");
+            Console.WriteLine("All rights to the Creator of the program");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+
+
+
+            if (!File.Exists(blacklistfilePath)) { Console.WriteLine("Error! Bitte lege eine blacklist.txt datei an im selben ordner wo das program selber liegt!"); Console.ReadKey(); return; }
+            if (!File.Exists(hashlistfilePath)) { Console.WriteLine("Error! Bitte lege eine hashlist.txt datei an im selben ordner wo das program selber liegt!"); Console.ReadKey(); return; }
+
+
+            var b = new BuildFileList();
+            var sw = new Stopwatch();
+            Console.WriteLine("Gib bitte eine festplatte an die durchsucht werden soll zb 'C:\\'");
+            path = Console.ReadLine();
+            Console.WriteLine("Sollen die dateien auf MD5 hash mit unserer Datenbank abgeglichen werden? j/n");
+          
+            if (Console.ReadLine() == "n")
+            {
+                md5YesOrNo = false;
+                Console.WriteLine("MD5 hash check deaktiviert");
+            }
+            else
+            {
+                md5YesOrNo = true;
+                Console.WriteLine("MD5 hash check aktiviert");
+            }
+
+            Console.WriteLine("Laden der dateipfade... Bitte warten! (Dies kann ein paar Minuten dauern)");
+            sw.Start();
+            var files = b.GetFiles();      
+            sw.Stop();
+
+            foreach(var fileName in files)
+            {
+                if (md5YesOrNo)
+                {
+                    try
+                    {
+                        foreach (string item in File.ReadAllLines(hashlistfilePath))
+                        {
+                            if (CalculateMD5(fileName.FullName) == item)
+                            {
+                                foundhacksbymd5.Add(fileName.FullName + " MD5: " + item);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "error.txt", e.ToString());
+                    }
+
+                    Console.WriteLine(fileName.FullName + " Hash: " + CalculateMD5(fileName.FullName));
+                }
+                else
+                {
+                    try
+                    {
+                        foreach (string item in File.ReadAllLines(blacklistfilePath))
+                        {
+                            if (fileName.FullName.Contains(item))
+                            {
+                                possibleHacks.Add(fileName.FullName);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "error.txt", e.ToString());
+                    }
+                    Console.WriteLine(fileName.FullName);
+                }
+            }
+
+            Console.WriteLine("\n\n\n{0} dateien in {1} sekunden geladen auf festplatte {2}", files.Count, sw.Elapsed.TotalSeconds, path);
+
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\n\n\n\n\n\n\nPotenzielle hacks gefunden {0}", possibleHacks.Count);           
+            foreach (string pHack in possibleHacks)
+            {
+
+                Console.WriteLine(pHack);
+            }
+
+
+            Console.WriteLine("\n\n\n\n\n\n\n\nMD5 check hacks gefunden {0}", foundhacksbymd5.Count);
+            foreach (string pHack in foundhacksbymd5)
+            {
+
+                Console.WriteLine(pHack);
+            }
+
+            Console.WriteLine("\n\n\nDas program ist fertig!");
+            Console.ReadLine();
+        }
+
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);              
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+
+        public class BuildFileList
+        {
+            public List<FileInfo> GetFiles()
+            {
+                var di = new DirectoryInfo(path);
+                var directories = di.GetDirectories();
+                var files = new List<FileInfo>();
+                foreach (var directoryInfo in directories)
+                {
+                    try
+                    {                      
+                        GetFilesFromDirectory(directoryInfo.FullName, files);                       
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                return files;
+            }
+
+            private void GetFilesFromDirectory(string directory, List<FileInfo> files)
+            {
+                var di = new DirectoryInfo(directory);
+                var fs = di.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+                files.AddRange(fs);
+                var directories = di.GetDirectories();
+                foreach (var directoryInfo in directories)
+                {
+                    try
+                    {                    
+                        GetFilesFromDirectory(directoryInfo.FullName, files);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GTA5HackDetector/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("GTA5HackDetector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GTA5HackDetector")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("166f5d8a-15cb-44dd-81cd-2455bec0f731")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
